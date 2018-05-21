118dc8bc523426b2213113bb2cf06fe7d532f93c
Todd Johnson <toddjohnson407@gmail.com>
2018-05-21 03:20:27 -0600

    linked pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/about'
-  get 'pages/contact'
-  get 'pages/home'
+  get 'about', to: 'pages#about'
+  get 'contact', to: 'pages#contact'
+  get 'home', to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
